adf1ed86e7b4fa48d3c93c67d139a40f1b7a84a5
abolabo <abolabo@gmail.com>
2017-01-31 20:47:38 +0200

    v1.3beta dev

---
+++ v1.3beta/code/extensions/developer_tools/admin/controller/task/developer_tools/language.php
@@ -0,0 +1,139 @@
+<?php
+/*------------------------------------------------------------------------------
+  $Id$
+
+  AbanteCart, Ideal OpenSource Ecommerce Solution
+  http://www.AbanteCart.com
+
+  Copyright © 2011-2016 Belavier Commerce LLC
+
+  This source file is subject to Open Software License (OSL 3.0)
+  License details is bundled with this package in the file LICENSE.txt.
+  It is also available at this URL:
+  <http://www.opensource.org/licenses/OSL-3.0>
+
+ UPGRADE NOTE:
+   Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+   versions in the future. If you wish to customize AbanteCart for your
+   needs please refer to http://www.AbanteCart.com for more information.
+------------------------------------------------------------------------------*/
+if (!defined('DIR_CORE') || !IS_ADMIN){
+	header('Location: static_pages/');
+}
+
+/**
+ * Class ControllerTaskDeveloperToolsLanguage
+ * @property ModelToolDeveloperTools $model_tool_developer_tools
+ */
+class ControllerTaskDeveloperToolsLanguage extends AController{
+	public $data = array ();
+
+	public function main(){	}
+
+	public function translate(){
+		list($task_id,$step_id,) = func_get_args();
+		$this->load->library('json');
+
+		if (!$task_id || !$step_id){
+			$error_text = 'Cannot run task step. Task_id (or step_id) has not been set.';
+			$this->_return_error($error_text);
+		}
+
+		$tm = new ATaskManager();
+		$step_info = $tm->getTaskStep($task_id, $step_id);
+
+		if ($step_info['sort_order'] == 1){
+			$tm->updateTask($task_id,
+					array (
+							'last_time_run' => date('Y-m-d H:i:s')
+					)
+			);
+		}
+
+		if (!$step_info){
+			$error_text = 'Cannot run task step. Looks like task_id ' . $task_id . ' does not contain step_id ' . $step_id;
+			$this->_return_error($error_text);
+		}
+
+		$tm->updateStep($step_id, array ('last_time_run' => date('Y-m-d H:i:s')));
+
+		if (!$step_info['settings']){
+			$error_text = 'Cannot run task step_id' . $step_id . '. Unknown settings for it.';
+			$this->_return_error($error_text);
+		}
+
+		$this->loadModel('tool/developer_tools');
+		//send emails in loop and update task's step info for restarting if step or task failed
+		$step_settings = $step_info['settings'];
+		$source_file = $step_settings['source_file'];
+		$destination_file = $step_settings['destination_file'];
+		$src_language_code = $step_settings['src_language_code'];
+		$dst_language_code = $step_settings['language_extension_code'];
+		$translation_method = $step_settings['translation_method'];
+
+		$definitions = $this->language->ReadXmlFile($source_file);
+		//for common xml file exclude translations
+		if(basename($destination_file) == $step_settings['language_extension_directory'].'.xml'){
+			$exclude_keys = array(
+					'code',
+					'direction',
+					'date_format_short',
+					'date_format_long',
+					'time_format',
+					'time_format_short',
+					'decimal_point',
+					'thousand_point');
+		}else{
+			$exclude_keys = array();
+		}
+		$new_definitions = array();
+		if($definitions){
+			foreach($definitions as $def_key=> $def_value){
+				if(in_array($def_key, $exclude_keys)){
+					$translate_result = $def_value;
+				}else{
+					$translate_result = $this->language->translate(
+							$src_language_code,
+							$def_value,
+							$dst_language_code,
+							$translation_method);
+				}
+				if($translate_result){
+					$new_definitions[$def_key] = $translate_result;
+				}
+			}
+		}
+
+		if ($new_definitions){
+			//write new definitions into xml
+			$step_result = $this->model_tool_developer_tools->saveLanguageXML($destination_file, $new_definitions);
+			if(!$step_result){
+				$this->messages->saveNotice('Developer Tools Notice of Translation ("' . $destination_file . '")', implode('<br>', $this->model_tool_developer_tools->error));
+			}
+
+			//update task details to show them at the end
+			$tm->updateTaskDetails($task_id,
+					array (
+						//set 1 as "admin"
+						'created_by' => 1,
+						'settings'   => array ()
+					));
+			$task_step_text_result = sizeof($new_definitions).' definitions have been translated.';
+		} else{
+			$step_result = false;
+		}
+
+		$tm->updateStep($step_id, array ('last_result' => $step_result));
+
+		if (!$step_result){
+			$this->_return_error('Some errors during step run. Empty definitions set was set.');
+		}
+
+		$this->response->setOutput(AJson::encode(array ('result' => $step_result, 'message' => $task_step_text_result)));
+	}
+
+	private function _return_error($error_text){
+		$this->response->setOutput(AJson::encode(array ('result' => false, 'message' => $error_text)));
+	}
+
+}
---
+++ v1.3beta/code/extensions/developer_tools/admin/model/tool/developer_tools_language.php
@@ -0,0 +1,185 @@
+<?php
+/*------------------------------------------------------------------------------
+  $Id$
+
+  AbanteCart, Ideal OpenSource Ecommerce Solution
+  http://www.AbanteCart.com
+
+  Copyright © 2015 Belavier Commerce LLC
+
+  This source file is subject to Open Software License (OSL 3.0)
+  License details is bundled with this package in the file LICENSE.txt.
+  It is also available at this URL:
+  <http://www.opensource.org/licenses/OSL-3.0>
+
+ UPGRADE NOTE:
+   Do not edit or add to this file if you wish to upgrade AbanteCart to newer
+   versions in the future. If you wish to customize AbanteCart for your
+   needs please refer to http://www.AbanteCart.com for more information.
+------------------------------------------------------------------------------*/
+if (!defined('DIR_CORE')){
+	header('Location: static_pages/');
+}
+
+/**
+ * Class ModelToolDeveloperToolsLanguage
+ * @property ModelLocalisationLanguage $model_localisation_language
+ */
+class ModelToolDeveloperToolsLanguage extends Model{
+	public $errors = array ();
+
+	/**
+	 * @param string $task_name
+	 * @param array $data
+	 * @return array|bool
+	 */
+	public function createTask($task_name, $data = array()){
+
+		if (!$task_name){
+			$this->errors[] = 'Can not to create task. Empty task name has been given.';
+		}
+		$this->load->model('localisation/language');
+		$language = $this->model_localisation_language->getLanguage($data['source_language']);
+		if(!$language){
+			$this->errors[] = 'No source language with ID '.$data['source_language'].' found in database!';
+			return false;
+		}
+
+		//get URIs of recipients
+		$xml_files = $this->_get_source_xml_files($language['directory']);
+		$task_controller = 'task/developer_tools/language/translate';
+
+		if (!$xml_files){
+			$this->errors[] = 'No xml-files found in directory "'.$language['directory'].'"!';
+			return false;
+		}
+
+		$total_files_count = sizeof($xml_files);
+
+
+
+		//timeout in seconds for one item translation
+		$time_per_file = 20;
+		$tm = new ATaskManager();
+
+		//create new task
+		$task_id = $tm->addTask(
+				array ('name'               => $task_name,
+				       'starter'            => 1, //admin-side is starter
+				       'created_by'         => $this->user->getId(), //get starter id
+				       'status'             => $tm::STATUS_READY,
+				       'start_time'         => date('Y-m-d H:i:s', mktime(0, 0, 0, date('m'), date('d') + 1, date('Y'))),
+				       'last_time_run'      => '0000-00-00 00:00:00',
+				       'progress'           => '0',
+				       'last_result'        => '1', // think all fine until some failed step will set 0 here
+				       'run_interval'       => '0',
+						//think that task will execute with some connection errors
+				       'max_execution_time' => ($total_files_count * $time_per_file * 2)
+				)
+		);
+		if (!$task_id){
+			$this->errors = array_merge($this->errors, $tm->errors);
+			return false;
+		}
+
+		$tm->updateTaskDetails($task_id,
+				array(
+					'created_by' => $this->user->getId(),
+					'settings'   => array(
+										'files_count' => $total_files_count
+										)
+				)
+		);
+
+		//create steps
+		$sort_order = 1;
+		foreach($xml_files as $section=>$files){
+			foreach($files as $xml_file){
+				if ($section == 'storefront'){
+					$destination_file = DIR_EXT . $data['extension_txt_id'] . '/' . $section . '/language/'.$data['language_extension_directory'].'/' . str_replace(DIR_STOREFRONT . 'language/'.$language['directory'].'/', '', $xml_file);
+				} else{
+					$destination_file = DIR_EXT . $data['extension_txt_id'] . '/' . $section . '/language/'.$data['language_extension_directory'].'/' . str_replace(DIR_APP_SECTION . 'language/'.$language['directory'].'/', '', $xml_file);
+				}
+
+				if(basename($destination_file) == $language['directory'].'.xml'){
+					$destination_file = dirname($destination_file).'/'.$data['language_extension_directory'].'.xml';
+
+				}
+
+				$settings = array (
+						'src_language_code'            => $language['code'],
+						'language_extension_code'      => $data['language_extension_code'],
+						'language_extension_directory' => $data['language_extension_directory'],
+						'translation_method'           => $data['translation_method'],
+						'source_file'                  => $xml_file,
+						'destination_file'             => $destination_file
+				);
+
+				$step_id = $tm->addStep(array (
+						'task_id'            => $task_id,
+						'sort_order'         => $sort_order,
+						'status'             => 1,
+						'last_time_run'      => '0000-00-00 00:00:00',
+						'last_result'        => '0',
+					//think that task will execute with some connection errors
+						'max_execution_time' => $time_per_file,
+						'controller'         => $task_controller,
+						'settings'           => $settings
+				));
+				$eta[$step_id] = $time_per_file;
+				$sort_order++;
+			}
+		}
+
+		$task_details = $tm->getTaskById($task_id);
+
+
+		if($task_details){
+			foreach($eta as $step_id => $estimate){
+				$task_details['steps'][$step_id]['eta'] = $estimate;
+				//remove settings from output json array. We will take it from database on execution.
+				unset($task_details['steps'][$step_id]['settings']);
+			}
+			return $task_details;
+		}else{
+			$this->errors[] = 'Can not to get task details for execution';
+			$this->errors = array_merge($this->errors,$tm->errors);
+			return false;
+		}
+
+	}
+
+
+	protected function _get_source_xml_files($src_language_directory = 'english'){
+		$src_language_directory = !$src_language_directory ? 'english' : $src_language_directory;
+		$output = array();
+
+		// seek storefront side
+		$language_dir = DIR_STOREFRONT . 'language/' . $src_language_directory;
+		$items = new RecursiveIteratorIterator(
+		    new RecursiveDirectoryIterator($language_dir, RecursiveDirectoryIterator::SKIP_DOTS)
+		);
+
+		foreach($items as $item) {
+			$filename = (string)$item;
+			if(pathinfo($filename,PATHINFO_EXTENSION) == 'xml'){
+				$output['storefront'][] = $filename;
+			}
+		}
+
+		$language_dir = DIR_APP_SECTION . 'language/' . $src_language_directory;
+		$items = new RecursiveIteratorIterator(
+		    new RecursiveDirectoryIterator($language_dir, RecursiveDirectoryIterator::SKIP_DOTS)
+		);
+
+		foreach($items as $item) {
+			$filename = (string)$item;
+			if(pathinfo($filename,PATHINFO_EXTENSION) == 'xml'){
+				$output['admin'][] = $filename;
+			}
+		}
+
+		return $output;
+	}
+
+}
